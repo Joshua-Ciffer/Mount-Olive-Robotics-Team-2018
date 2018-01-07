9b6d45c3eed6f8f155bfb84185ac1056eed83a6c
Benny Mirisola <bennymirisola@gmail.com>
2018-01-06 21:52:33 -0500

    Initial Commit

---
+++ src/main/java/org/mort11/Robot.java
@@ -0,0 +1,45 @@
+package org.mort11;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+/**
+ * Created by Benny Mirisola on 1/6/18.
+ */
+public class Robot extends IterativeRobot {
+
+    @Override
+    public void robotInit() {
+        super.robotInit();
+
+    }
+
+    @Override
+    public void teleopInit() {
+        super.teleopInit();
+    }
+
+    @Override
+    public void autonomousPeriodic() {
+        super.autonomousPeriodic();
+    }
+
+    @Override
+    public void teleopPeriodic() {
+        super.teleopPeriodic();
+    }
+
+    @Override
+    public void autonomousInit() {
+        super.autonomousInit();
+    }
+
+    @Override
+    public void testPeriodic() {
+        super.testPeriodic();
+    }
+
+    @Override
+    public void testInit() {
+        super.testInit();
+    }
+}
