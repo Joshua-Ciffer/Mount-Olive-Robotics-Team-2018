48811df49bf269d05a7bbd9a4e2fdabfbb6d1359
Benny Mirisola <bennymirisola@gmail.com>
2018-01-30 00:26:15 -0500

    added gitignore

--- src/main/java/org/mort11/Hardware/IO.java
+++ src/main/java/org/mort11/Hardware/IO.java
@@ -1,7 +1,15 @@
 package org.mort11.Hardware;
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
 /**
  * Created by Benny Mirisola on 1/10/18.
  */
 public class IO {
+
+    private TalonSRX leftTalonMaster;
+    private TalonSRX leftTalonFollower;
+    private TalonSRX rightTalonMaster;
+    private TalonSRX rightTalonFollower;
+
 }
