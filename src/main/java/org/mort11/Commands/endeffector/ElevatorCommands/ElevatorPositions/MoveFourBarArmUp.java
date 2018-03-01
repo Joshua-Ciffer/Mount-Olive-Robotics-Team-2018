a998ae2235e140093e77d128d50457a4cdcf48c1
Timothy Vu <timothy.vu@motsd.org>
2018-03-01 11:11:17 -0500

    fixed stuff with the four bar arm limit switch code

--- src/main/java/org/mort11/Commands/endeffector/ElevatorCommands/ElevatorPositions/MoveFourBarArmUp.java
+++ src/main/java/org/mort11/Commands/endeffector/ElevatorCommands/ElevatorPositions/MoveFourBarArmUp.java
@@ -1,7 +1,9 @@
 package org.mort11.Commands.endeffector.ElevatorCommands.ElevatorPositions;
 
 import edu.wpi.first.wpilibj.command.Command;
+import org.mort11.Hardware.IO;
 import org.mort11.Robot;
+import org.mort11.Util.Constants;
 
 public class MoveFourBarArmUp extends Command {
     private double speed;
@@ -11,12 +13,12 @@ public class MoveFourBarArmUp extends Command {
     }
     @Override
     protected void execute() {
-        if(speed < 0)
+        if(speed < -Constants.MOTOR_DEADZONE)
             Robot.fourBarArm.set(speed);
     }
     @Override
     protected boolean isFinished() {
-        return false;
+        return IO.getActuatorLimitSwitchTop().get() || speed > Constants.MOTOR_DEADZONE;
     }
     @Override
     protected void end() {
