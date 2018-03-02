6bf4361ebbe782b76eb87117675cc30dba8e417b
Frank Alfano <q24xboss@gmail.com>
2018-03-02 02:32:31 -0500

    Fixed the second stage elevator joystick drive commands

--- src/main/java/org/mort11/Commands/endeffector/ElevatorCommands/ElevatorPositions/MoveSecondStageElevatorDown.java
+++ src/main/java/org/mort11/Commands/endeffector/ElevatorCommands/ElevatorPositions/MoveSecondStageElevatorDown.java
@@ -1,21 +1,23 @@
 package org.mort11.Commands.endeffector.ElevatorCommands.ElevatorPositions;
 
 import edu.wpi.first.wpilibj.command.Command;
+import org.mort11.Control.Operator;
 import org.mort11.Hardware.IO;
 import org.mort11.Robot;
 import org.mort11.Util.Constants;
 
 public class MoveSecondStageElevatorDown extends Command{
     private double speed;
-    public MoveSecondStageElevatorDown(double speed){
+    public MoveSecondStageElevatorDown(){
         super("JoystickDriveSecondStageElevatorDown");
         requires(Robot.secondStageElevator);
         setInterruptible(true);
-        this.speed = speed;
     }
 
     @Override
     protected void execute() {
+        speed = Operator.getRightOperatorJoystick().getY();
+
         if(speed > Constants.MOTOR_DEADZONE) {
             Robot.secondStageElevator.set(speed);
         }
