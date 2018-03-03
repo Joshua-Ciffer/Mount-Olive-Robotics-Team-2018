84e8ecd859b066bd1adf65c780193db0d49da9c6
Frank Alfano <q24xboss@gmail.com>
2018-03-03 09:32:57 -0500

    added switch statements in the elevator set commands

--- src/main/java/org/mort11/Commands/endeffector/ElevatorCommands/ElevatorPositions/auton/MoveFirstStageElevatorSetValue.java
+++ src/main/java/org/mort11/Commands/endeffector/ElevatorCommands/ElevatorPositions/auton/MoveFirstStageElevatorSetValue.java
@@ -1,20 +1,71 @@
 package org.mort11.Commands.endeffector.ElevatorCommands.ElevatorPositions.auton;
 
 import edu.wpi.first.wpilibj.command.TimedCommand;
+import org.mort11.Hardware.HardwareStates;
+import org.mort11.Hardware.IO;
 import org.mort11.Robot;
 
 public class MoveFirstStageElevatorSetValue extends TimedCommand{
     private double speed;
-    public MoveFirstStageElevatorSetValue(double speed, double timeout){
+    private HardwareStates.ElevatorDirection elevatorDirection;
+    public MoveFirstStageElevatorSetValue(double speed, HardwareStates.ElevatorDirection elevatorDirection, double timeout){
         super(timeout);
         requires(Robot.firstStageElevator);
-        setInterruptible(false);
+        setInterruptible(true);
         this.speed = speed;
+        this.elevatorDirection = elevatorDirection;
     }
 
     @Override
     protected void execute() {
-        Robot.firstStageElevator.set(speed);
+        switch (elevatorDirection) {
+            case UP:
+                if (IO.getFirstStageElevatorLimitSwitchTop().get()) {
+
+                    Robot.firstStageElevator.set(speed);
+
+                }
+
+                break;
+            case DOWN:
+
+                if (IO.getFirstStageElevatorLimitSwitchBottom().get()) {
+
+                    Robot.firstStageElevator.set(-speed);
+
+                }
+
+                break;
+        }
+    }
+
+    @Override
+    protected boolean isFinished() {
+
+        super.isFinished();
+
+        switch (elevatorDirection) {
+            case UP:
+                if (IO.getFirstStageElevatorLimitSwitchTop().get()) {
+
+                    return true;
+
+                }
+
+                break;
+            case DOWN:
+
+                if (IO.getFirstStageElevatorLimitSwitchBottom().get()) {
+
+                    return true;
+
+                }
+
+                break;
+        }
+
+        return false;
+
     }
 
     @Override
