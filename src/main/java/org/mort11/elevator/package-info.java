db404e66395ba95e06a91c77d4f3cb08527abcda
Joshua Ciffer <Joshua_Ciffer@Outlook.com>
2018-06-08 09:24:09 -0400

    Wrote javadoc.

--- src/main/java/org/mort11/elevator/package-info.java
+++ src/main/java/org/mort11/elevator/package-info.java
@@ -1,7 +1,7 @@
 /**
- * 
+ * This package contains all code for the elevator subsystem and its related commands.
  *
- * @author Joshua
- * @version 05/31/2018
+ * @author Joshua Ciffer
+ * @version 06/08/2018
  */
 package org.mort11.elevator;
