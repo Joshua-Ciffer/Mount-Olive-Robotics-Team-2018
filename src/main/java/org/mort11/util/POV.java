d104d09340f0d527dfcbd005b62822e7ec76cd73
Joshua Ciffer <Joshua_Ciffer@Outlook.com>
2018-06-13 12:59:31 -0400

    Thank you Matt Turi for this beautifully written code.

--- src/main/java/org/mort11/util/POV.java
+++ src/main/java/org/mort11/util/POV.java
@@ -1,13 +1,13 @@
 package org.mort11.util;
 
 public enum POV {
-    NONE(-1), UP(0), UP_RIGHT(45), RIGHT(90), DOWN_RIGHT(135), DOWN(180), DOWN_LEFT(225), LEFT(270), UP_LEFT(315);
 
-    public int direction;
+	NONE(-1), UP(0), UP_RIGHT(45), RIGHT(90), DOWN_RIGHT(135), DOWN(180), DOWN_LEFT(225), LEFT(270), UP_LEFT(315);
 
-    POV(int direction) {
-        this.direction = direction;
-    }
+	public int direction;
+
+	POV(int direction) {
+		this.direction = direction;
+	}
 
 }
-
