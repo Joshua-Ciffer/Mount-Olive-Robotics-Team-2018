23bd327f8d6acfffba4db0bd24a17fa86e44f514
Joshua Ciffer <Joshua_Ciffer@Outlook.com>
2018-06-12 23:12:25 -0400

    Setup API GitHub page.

--- src/main/java/org/mort11/led/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * This package contains all code for the LED subsystem and its associated commands.
- *
- * @author Joshua Ciffer
- * @version 05/31/2018
- */
-package org.mort11.led;
--- src/main/java/org/mort11/util/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * This package contains project wide resources and values.
- *
- * @author Joshua Ciffer
- * @version 06/12/2018
- */
-package org.mort11.util;
