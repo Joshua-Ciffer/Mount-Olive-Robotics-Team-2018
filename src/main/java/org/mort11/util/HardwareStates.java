7f2dae7b8464b7deb63dfb41ee257d9d5720c3d4
Joshua Ciffer <Joshua_Ciffer@Outlook.com>
2018-06-13 01:08:51 -0400

    Documented hardware states.

--- src/main/java/org/mort11/util/HardwareStates.java
+++ src/main/java/org/mort11/util/HardwareStates.java
@@ -3,40 +3,110 @@ package org.mort11.util;
 /**
  * Contains enum type states for different hardware conditions.
  *
- * @author Zach "Merge Errors" Rich
  * @author Joshua Ciffer
- * @version 02/14/2018
+ * @author Zach "Merge Errors" Rich
+ * @version 06/13/2018
  */
-public class HardwareStates {
+public final class HardwareStates {
 
-    /**
-     * States for the first stage elevator.
-     */
-    public enum ElevatorState {
-        BOTTOM, TOP
-    }
+	/**
+	 * Don't let anyone instantiate this class.
+	 */
+	private HardwareStates() {}
 
-    /**
-     * States for the intake piston.
-     */
-    public enum IntakePistonState {
-        IN, OUT
-    }
+	/**
+	 * Hardware states for the elevator.
+	 */
+	public enum ElevatorState {
 
-    public enum IntakeShiftState {
-        UP, DOWN
-    }
+		/**
+		 * State when the elevator cannot go any lower (bottom elevator limit switch is pressed).
+		 */
+		BOTTOM,
 
-    public enum IntakeRollersState {
-        IN, OUT, STOP, COAST
-    }
+		/**
+		 * State when the elevator cannot go any higher (top elevator limit switch is pressed).
+		 */
+		TOP
 
-    public enum DrivetrainGear {
-        HIGH, LOW
-    }
+	}
 
-    public enum ElevatorDirection {
-        UP, DOWN
+	/**
+	 * Hardware states for the intake piston.
+	 */
+	public enum IntakePistonState {
 
-    }
+		/**
+		 * State when the intake is closed (holding on to a cube).
+		 */
+		IN,
+
+		/**
+		 * State when the intake is open (releasing a cube).
+		 */
+		OUT
+
+	}
+
+	/**
+	 * Hardware states for the intake shifter.
+	 */
+	public enum IntakeShiftState {
+
+		/**
+		 * State when the intake is shifted in the up position.
+		 */
+		UP,
+
+		/**
+		 * State when the intake is shifted in the down position.
+		 */
+		DOWN
+
+	}
+
+	/**
+	 * Hardware states for the intake rollers.
+	 */
+	public enum IntakeRollersState {
+
+		/**
+		 * State when the intake is intaking a cube.
+		 */
+		IN,
+
+		/**
+		 * State when the intake is outtaking a cube.
+		 */
+		OUT,
+
+		/**
+		 * State when the intake rollers are stopped.
+		 */
+		STOP,
+
+		/**
+		 * State when the intake rollers are coasting.
+		 */
+		COAST
+
+	}
+
+	/**
+	 * Hardware states for the drivetrain transmission.
+	 */
+	public enum DrivetrainGear {
+
+		/**
+		 * State when the drivetrain is in high gear.
+		 */
+		HIGH,
+
+		/**
+		 * State when the drivetrain is in low gear.
+		 */
+		LOW
+
+	}
+
 }
