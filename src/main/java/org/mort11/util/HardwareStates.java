b2205c88eb09bb815b086e6bcef159d4531dccf3
Joshua Ciffer <Joshua_Ciffer@Outlook.com>
2018-11-08 20:31:32 -0500

    Deleted unecessary hardware states

--- src/main/java/org/mort11/util/HardwareStates.java
+++ src/main/java/org/mort11/util/HardwareStates.java
@@ -14,23 +14,6 @@ public final class HardwareStates {
 	private HardwareStates() {}
 
 	/**
-	 * Hardware states for the elevator.
-	 */
-	public enum ElevatorState {
-
-		/**
-		 * State when the elevator cannot go any lower (bottom elevator limit switch is pressed).
-		 */
-		BOTTOM,
-
-		/**
-		 * State when the elevator cannot go any higher (top elevator limit switch is pressed).
-		 */
-		TOP
-
-	}
-
-	/**
 	 * Hardware states for the intake rollers.
 	 */
 	public enum IntakeRollersState {
